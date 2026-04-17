afecba73870bd7560f772d40853e1ce3687e5577
Vitor Acer5 <vitor.venante@gmail.com>
2026-04-16 21:37:51 -0300

    Aprendendo Classe em .Java

--- fundProgJava/pooJava/classeEncapsulamento/Main.java
+++ fundProgJava/pooJava/classeEncapsulamento/Main.java
@@ -1,7 +1,17 @@
 package pooJava.classeEncapsulamento;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+           
+        var pessoa = new Pessoa();
+
+        pessoa.setNome("Vitor");
+        pessoa.setIdade(27);
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
         
     }
 }
