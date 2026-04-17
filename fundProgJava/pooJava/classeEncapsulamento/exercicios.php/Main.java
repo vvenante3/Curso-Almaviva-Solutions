b6284c4062d2abe95f3f267fa1314238a8fd4f97
Vitor Acer5 <vitor.venante@gmail.com>
2026-04-16 22:44:58 -0300

    inicio prática

---
+++ fundProgJava/pooJava/classeEncapsulamento/exercicios.php/Main.java
@@ -0,0 +1,21 @@
+public class Main{
+
+    /*
+    
+    Crie uma classe ContaBancaria com:
+
+        saldo (double)
+        titular (String)
+
+    Regras:
+
+        - saldo não pode ser negativo
+        - construtor deve obrigar titular e saldo inicial
+        - método para depositar
+        - método para sacar (não pode deixar saldo negativo)
+    
+    */
+    public static void main(String[] args) {
+        
+    }
+}
